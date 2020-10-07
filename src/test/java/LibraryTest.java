f49a0269415e1b4cbef414915bee66cdbff57cd9
kumarbhupi <kumar.bhupi13@gmail.com>
2020-10-07 20:16:15 +0200

    New test created

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -5,5 +5,6 @@ public class LibraryTest extends TestCase {
   public void testBookBook() {
     Library library = new Library();
     assert library.bookBook();
+    assert !library.bookBook();
   }
 }
